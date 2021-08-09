92f8773c96b6dddf5fe55fe7ecff5cdd9f8a68cb
1974258963 <1974258963@qq.com>
2021-08-09 14:29:37 +0800

    新增属性修改值：model, click, bind, html

--- src/view.js
+++ src/view.js
@@ -34,13 +34,54 @@ View.prototype = {
             }else if(node.nodeType === 3 && reg.test(content)){ // text
                 this.compileText(node, reg.exec(content)[1]);
             }
-            if(childNodes?.childNodes.length){
+            if(node?.childNodes?.length){
                 this.compileElement(node)
             }
         })
     },
     elementCompile(node){
-
+        const attributes = node.attributes;
+        const that = this;
+        Array.prototype.forEach.call(attributes, (attr) => {
+            let attrName = attr.name;
+            if(attrName.indexOf('v-') !== 0){
+                return;
+            }
+            if(attrName.indexOf('v-model') === 0){
+                that.elementCompileModel(node, attr, this.vm);
+            }else if(attrName.indexOf('v-html') === 0){
+                that.elementCompileHtml(node, attr, this.vm);
+            }else if(attrName.indexOf('v-bind') === 0){
+                that.elementCompileBind(node, attr, this.vm);
+            }else if(attrName.indexOf('v-on:click') === 0){
+                that.elementCompileEvent(node, attr, this.vm);
+            }else{
+                console.error(`无法识别${attrName}`)
+            }
+        })
+    },
+    elementCompileModel(node, attr, vm){
+        const value = attr.value;
+        node.value = vm.data[value];
+        node.removeAttribute(attr.name);
+    },
+    elementCompileBind(node, attr, vm){
+        const name = attr.name;
+        const value = attr.value;
+        const reg = /\:(.*)/;
+        const bindName = reg.exec(name)[1];
+        node[bindName] = vm.data[value]
+    },
+    elementCompileHtml(node, attr, vm){
+        node.removeAttribute(attr.name);
+    },
+    elementCompileEvent(node, attr, vm){
+        const value = attr.value;
+        const handle = vm.methods && vm.methods[value];
+        if(handle){
+            node.addEventListener('click', handle.bind(vm), false);
+        }
+        node.removeAttribute(attr.name);
     },
     compileText(node, exp){
         const text = this.vm[exp];
